b1073c76d2eaf7b7e645ffd047b9e6825e95100d
arun.kumar <arun.kumar@alethelabs.co.in>
2023-03-26 17:04:31 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { IndexPageComponent } from './index-page/index-page.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ServicesOfferComponent } from './services-offer/services-offer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Index', component: IndexPageComponent },
+  { path: 'services', component: ServicesOfferComponent ,
+  children: [
+    {
+      path: 'index-services', // child route path
+      component: ServicesOfferComponent, // child route component that the router renders
+    }
+  ],  
+},
+  { path: 'about', component: AboutUsComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: '',   redirectTo: '/Index', pathMatch: 'full' }, // redirect to `first-component`
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexPageComponent } from './index-page/index-page.component';
+import { ServicesOfferComponent } from './services-offer/services-offer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexPageComponent,
+    ServicesOfferComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
